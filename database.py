7f18ddbe63e994b24a27c0e8944d7519862800cc
arun venkatesh Naik <154398424+arunvnaik@users.noreply.github.com>
2025-06-23 16:59:13 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
+# Import List from typing to define a list of Book objects
 from typing import List
+
+# Import the Book Pydantic model (used to define the data structure)
 from models import Book
 
+# A fake database using a simple list that stores Book objects
+# In real-world apps, this would be replaced by a real database like PostgreSQL
 books_db: List[Book] = []
